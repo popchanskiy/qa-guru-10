f326cc4cfd38815d7a87f81e324a7e72a1cfdf18
popchanskiy <popov.ilya1240@gmail.com>
2021-12-29 16:19:36 +0300

    add GitHub search test

---
+++ src/test/java/guru/qa/lesson3/task2/ExploringSelenideRepo.java
@@ -0,0 +1,80 @@
+package guru.qa.lesson3.task2;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.exist;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class ExploringSelenideRepo {
+    /*- Откройте страницу Selenide в Github
+
+     - Перейдите в раздел Wiki проекта
+
+     - Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+
+     - Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5*/
+    private ExploringSelenideRepo exploringSelenideRepo;
+
+    @BeforeAll
+    static void configurateTestRun() {
+        //Configuration.browserSize="1920*1080";
+        open("https://github.com/");
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @BeforeEach
+    void init() {
+        exploringSelenideRepo = new ExploringSelenideRepo();
+    }
+
+    @Test
+    void SelenideRepoContainsJunit5ExampleOnSoftAssertionsPage() {
+        exploringSelenideRepo.searchRepo("Selenide");
+        exploringSelenideRepo.chooseRepoByName("/selenide/selenide");
+        exploringSelenideRepo.goToRepoSection("Wiki");
+        exploringSelenideRepo.pagePresenting("SoftAssertions","/selenide/selenide/wiki/SoftAssertions");
+        exploringSelenideRepo.textPresentingInsidePage("Junit5");
+    }
+
+
+    void searchRepo(String repoName) {
+        $("[data-test-selector=nav-search-input]")
+                .setValue(repoName)
+                .pressEnter();
+    }
+
+    private void chooseRepoByName(String repoName) {
+        $("a[href=" + "\'" + repoName + "\'" + "]")
+                .click();//наверное стоило экранировать '
+    }
+
+    void goToRepoSection(String repoSectionName) {
+        $("span[data-content=" + "\'" + repoSectionName + "\'" + "]")
+                .click();
+    }
+
+    void pagePresenting(String pageName,String refName) {
+       /* $(".markdown-body").$("[href='/selenide/selenide/wiki/" +
+                        "\'"+ pageName+ "\'"+ "]");
+
+        */
+        $$("a[class='internal present']").find(Condition.text(pageName));
+        $(".markdown-body").$("[href=" +"\'" + refName + "\'"+ "]").click();
+
+    }
+
+    void textPresentingInsidePage(String text) {
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
+    }
+}
+
+
